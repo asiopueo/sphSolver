b892ec4b93b4c640aca1a6edcc5f02668ff06572
Martin <martin.lippl@pueo.de>
2019-06-07 21:14:41 +0200

    Slight modification of the compile script to make it work.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 g++ main.c common.c physics.c loader.c memory.c -o ./sph_alpha -Wall -g -I/usr/include/freetype2 \
 	-L/usr/lib/nvidia-346 -L/usr/lib/x86_64-linux-gnu/ -L/usr/local/lib \
-	-lglfw3 -lGLEW -lGLU -lGL -lXxf86vm -lXrandr -lpthread -lXi -lXinerama -lX11 -lXcursor -lfreetype -lSOIL
+	-lglfw -lGLEW -lGLU -lGL -lXxf86vm -lXrandr -lpthread -lXi -lXinerama -lX11 -lXcursor -lfreetype -lSOIL
