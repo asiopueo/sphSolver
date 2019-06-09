2aa105f7371a04455ee9adc21a336916d05e3afa
Martin <martin.lippl@pueo.de>
2019-06-09 21:34:21 +0200

    Replaced water refraction shaders with the ones from the Stanford bunny demonstration (branch PureGraphics)

--- shaders/water_refrac.fs
+++ shaders/water_refrac.fs
@@ -3,15 +3,22 @@
 in vec3 Normal;
 in vec3 Position;
 
-out vec3 color;
+out vec4 color;
 
 uniform vec3 cameraPos;
 uniform samplerCube skybox;
 
 void main()
 {
-		vec3 I = normalize(Position - cameraPos);
-		vec3 R = reflect(I, normalize(Normal));
-		color = texture(skybox, R);
+	/*
+	vec3 I = vec3(normalize(Position - cameraPos));
+	vec3 R = vec3(reflect(I, normalize(Normal)));
+	color = texture(skybox, R);
+	*/
+	float ratio = 1.00 / 1.52;
+    vec3 I = normalize(Position - cameraPos);
+    vec3 R = refract(I, normalize(Normal), ratio);
+    color = texture(skybox, R);
 }
+
 
--- shaders/water_refrac.vs
+++ shaders/water_refrac.vs
@@ -11,7 +11,7 @@ uniform mat4 ProjectionMatrix;
 
 void main()
 {
-		gl_Position = ProjectionMatrix * ViewMatrix * vec4(position, 1.0);
-		Normal = mat3(transpose(inverse(ModelMatrix)));
-		Position = vec3(ModelMatrix * vec4(postion, 1.0f));
+		gl_Position = ProjectionMatrix * ViewMatrix * ModelMatrix * vec4(position, 1.0);
+		Normal = vec3(transpose(inverse(ModelMatrix)) * vec4(normal, 1.0));
+		Position = vec3(ModelMatrix * vec4(position, 1.0f));
 }
