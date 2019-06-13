6ad00bda0ef41043787a9db5c84dabd5a0d9fc9b
Martin <martin.lippl@pueo.de>
2019-06-14 01:05:37 +0200

    Interchanged linker sequence for object files in compile.sh and debug.sh scripts. Will remove these temporary scripts soon.

--- debug.sh
+++ debug.sh
@@ -1,3 +1,3 @@
-g++ -g src/main.c src/common.c src/marchingcubes.c src/physics.c src/loader.c src/memory.c -o ./build/sph_alpha -Wall -g -I/usr/include/freetype2 \
+g++ -g src/main.c src/common.c src/physics.c src/density.c src/loader.c src/marchingcubes.c src/memory.c -o ./build/sph_alpha -Wall -g -I/usr/include/freetype2 \
 	-L/usr/lib/nvidia-346 -L/usr/lib/x86_64-linux-gnu/ -L/usr/local/lib \
 	-lglfw -lGLEW -lGLU -lGL -lXxf86vm -lXrandr -lpthread -lXi -lXinerama -lX11 -lXcursor -lfreetype -lSOIL
