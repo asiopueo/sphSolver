8d0e3bd90bf657f1c76d101229cf8c7d8578ff26
Martin <martin.lippl@pueo.de>
2020-08-15 00:54:25 +0200

    Some general cleanup and deletion of various compile and debug files. Setting the stage for reconfiguration of cmake file.

--- compile.sh
+++
@@ -1,3 +0,0 @@
-g++ src/main.c src/common.c src/physics.c src/density.c src/shaderLoader.c src/textureLoader.c src/marchingcubes.c src/renderer.c src/camera.c -o ./build/sph_alpha -Wall -g -I/usr/include/freetype2 \
-	-L/usr/lib/nvidia-346 -L/usr/lib/x86_64-linux-gnu/ -L/usr/local/lib \
-	-lglfw -lGLEW -lGLU -lGL -lXxf86vm -lXrandr -lpthread -lXi -lXinerama -lX11 -lXcursor -lfreetype -lSOIL
